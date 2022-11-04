f3502071f9496dba38134039c89389156bf60c26
Yannik Hodel <yannik.hodel@gmail.com>
2022-11-04 15:14:09 +0100

    Final scene rendering

--- Raytracing-In-A-Weekend/src/Raytracer.cpp
+++ Raytracing-In-A-Weekend/src/Raytracer.cpp
@@ -40,36 +40,73 @@ void writeColor(std::ostream& out, glm::vec3 pixelColor, int samplesPerPixel) {
 		<< static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
+HittableList randomScene() {
+	HittableList world;
+
+	auto groundMaterial = std::make_shared<Lambertian>(glm::vec3(0.5f, 0.5f, 0.5f));
+	world.add(std::make_shared<Sphere>(glm::vec3(0.0f, -1000.0f, 0.0f), 1000.0f, groundMaterial));
+
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto chooseMat = randomFloat();
+			glm::vec3 center(a + 0.9f * randomFloat(), 0.2f, b + 0.9f * randomFloat());
+
+			if ((center - glm::vec3(4.0f, 0.2f, 0.0f)).length() > 0.9f) {
+				shared_ptr<Material> sphereMaterial;
+
+				if (chooseMat < 0.8) {
+					// diffuse
+					auto albedo = randomVec() * randomVec();
+					sphereMaterial = std::make_shared<Lambertian>(albedo);
+					world.add(std::make_shared<Sphere>(center, 0.2f, sphereMaterial));
+				}
+				else if (chooseMat < 0.95f) {
+					// metal
+					auto albedo = randomVec(0.5f, 1.0f);
+					auto fuzz = randomFloat(0.0f, 0.5f);
+					sphereMaterial = std::make_shared<Metal>(albedo, fuzz);
+					world.add(std::make_shared<Sphere>(center, 0.2f, sphereMaterial));
+				}
+				else {
+					// glass
+					sphereMaterial = std::make_shared<Dielectric>(1.5f);
+					world.add(std::make_shared<Sphere>(center, 0.2f, sphereMaterial));
+				}
+			}
+		}
+	}
+
+	auto material1 = make_shared<Dielectric>(1.5f);
+	world.add(make_shared<Sphere>(glm::vec3(0.0f, 1.0f, 0.0f), 1.0f, material1));
+
+	auto material2 = make_shared<Lambertian>(glm::vec3(0.4f, 0.2f, 0.1f));
+	world.add(make_shared<Sphere>(glm::vec3(-4.0f, 1.0f, 0.0f), 1.0f, material2));
+
+	auto material3 = make_shared<Metal>(glm::vec3(0.7f, 0.6f, 0.5f), 0.0f);
+	world.add(make_shared<Sphere>(glm::vec3(4.0f, 1.0f, 0.0f), 1.0f, material3));
+
+	return world;
+}
+
 int main()
 {
 	//Image
-	const float aspectRatio = 16.0f / 9.0f;
-	const int imageWidth = 400;
+	const float aspectRatio = 3.0f / 2.0f;
+	const int imageWidth = 1200;
 	const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
-	const int samplesPerPixel = 100;
+	const int samplesPerPixel = 500;
 	const int maxDepth = 50;
 
-	//Materials
-	auto materialGround = make_shared<Lambertian>(glm::vec3(0.8f, 0.8f, 0.0f));
-	auto materialCenter = make_shared<Lambertian>(glm::vec3(0.1f, 0.2f, 0.5f));
-	auto materialLeft = make_shared<Dielectric>(1.5f);
-	auto materialRight = make_shared<Metal>(glm::vec3(0.8f, 0.6f, 0.2f), 0.0f);
-
 	//World
-	auto R = cos(pi / 4);
-	HittableList world;
-	world.add(make_shared<Sphere>(glm::vec3(0.0f, -100.5f, -1.0f), 100.0f, materialGround));
-	world.add(make_shared<Sphere>(glm::vec3(0.0f, 0.0f, -1.0f), 0.5f, materialCenter));
-	world.add(make_shared<Sphere>(glm::vec3(-1.0f, 0.0f, -1.0f), 0.5f, materialLeft));
-	world.add(make_shared<Sphere>(glm::vec3(-1.0f, 0.0f, -1.0f), -0.45f, materialLeft));
-	world.add(make_shared<Sphere>(glm::vec3(1.0f, 0.0f, -1.0f), 0.5f, materialRight));
+	HittableList world = randomScene();
+
 
 	//Camera
-	glm::vec3 lookfrom = { 3.0f, 3.0f, 2.0f };
-	glm::vec3 lookat = { 0.0f, 0.0f, -1.0f };
+	glm::vec3 lookfrom = { 13.0f, 2.0f, 3.0f };
+	glm::vec3 lookat = { 0.0f, 0.0f, 0.0f };
 	glm::vec3 vup = { 0.0f, 1.0f, 0.0f };
-	float distToFocus = glm::length(lookfrom - lookat);
-	float aperture = 2.0f;
+	float distToFocus = 10.0f;
+	float aperture = 0.1f;
 	Camera cam(lookfrom, lookat, vup, 20.0f, aspectRatio, aperture, distToFocus);
 
 	//Render
